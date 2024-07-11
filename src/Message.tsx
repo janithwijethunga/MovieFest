7d7de67f8417ff2aa565c1d72dd432067eb752bf
janithwijethunga <janithwijethunga68@gmail.com>
2024-07-11 11:49:06 +0530

    updated

--- src/Message.tsx
+++ src/Message.tsx
@@ -4,10 +4,11 @@ function Message() {
   const name = "Janith";
   return (
     <div className='p-4'>
-      <h1 className="text-red-400, border-2">Hello {name}!</h1>
+      <h1 className="">Hello {name}!</h1>
       <p>How are you?</p>
     </div>
   );
 }
 
 export default Message;
+
